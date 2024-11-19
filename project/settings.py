295a7b072ebdf0ce9510ae118dffdffd18ad7537
Mick Byrne <mick.byrne@splend.com>
2024-11-19 21:13:01 +0000

    Got ChatGPT to prettify the forms.

--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages',
-    'posts'
+    'posts',
+    'accounts',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,5 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
